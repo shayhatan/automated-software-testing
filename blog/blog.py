60f89c4a20cac2dc7bcf3add2ac6139ca66c1266
shayhatan <shayhatan7@gmail.com>
2025-08-12 12:00:30 +0300

    chore(git): stop tracking __pycache__

--- blog/blog.py
+++ blog/blog.py
@@ -6,7 +6,8 @@ class Blog:
         self.posts = []
 
     def __repr__(self):
-        pass
+        return '{} by {} ({} post{})'.format(self.title, self.author, len(self.posts),
+                                              's' if len(self.posts) != 1 else '')
 
     def create_post(self, title, content):
         pass
