4feee85c8f1b6749229c5874f7c7bfdd892b7ebf
shayhatan <shayhatan7@gmail.com>
2025-08-12 10:40:48 +0300

    feat(blog): add Post serialization (to_dict, to_json) with unit tests

--- blog/tests/unit/test_post.py
+++ blog/tests/unit/test_post.py
@@ -7,3 +7,11 @@ class PostTest(TestCase):
         p = Post('Test', 'Test Content')
         self.assertEqual('Test', p.title)
         self.assertEqual('Test Content', p.content)
+
+    def test_json(self):
+        p = Post('Test', 'Test Content')
+        expected = {'title': 'Test', 'content': 'Test Content'}
+
+        # can not use assertEqual because it will check dict obj is exactly the same, but they may be diff
+        self.assertDictEqual(expected, p.json())
+
