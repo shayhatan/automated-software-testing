e32259bd9f84f56d3dc57b8f6e2e6f2b299df2e4
shayhatan <shayhatan7@gmail.com>
2025-08-12 12:47:57 +0300

    feat(blog): create_post + JSON serialization; add integration test

--- blog/blog.py
+++ blog/blog.py
@@ -1,3 +1,5 @@
+from blog.post import Post
+
 
 class Blog:
     def __init__(self, title, author):
@@ -10,7 +12,11 @@ class Blog:
                                               's' if len(self.posts) != 1 else '')
 
     def create_post(self, title, content):
-        pass
+        self.posts.append(Post(title, content))
 
     def json(self):
-        pass
+        return {
+            'title': self.title,
+            'author': self.author,
+            'posts': [post.json() for post in self.posts]
+        }
