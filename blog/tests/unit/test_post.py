b7ce64c9e4a39aac63a40359ada167b0479bc694
shayhatan <shayhatan7@gmail.com>
2025-08-12 10:24:56 +0300

    feat(blog): add Post and unit test for creation

---
+++ blog/tests/unit/test_post.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from blog.post import Post
+
+
+class PostTest(TestCase):
+    def test_create_post(self):
+        p = Post('Test', 'Test Content')
+        self.assertEqual('Test', p.title)
+        self.assertEqual('Test Content', p.content)
