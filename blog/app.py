0ce2658122beb9ceb5d4980ca9c81e34f71a6148
shayhatan <shayhatan7@gmail.com>
2025-08-13 08:50:40 +0300

    feat(app): implement menu, print_blogs, ask_create_blog/read_blog, print_posts/print_post

--- blog/app.py
+++ blog/app.py
@@ -1,12 +1,52 @@
+from blog.blog import Blog
+
+MENU_PROMPT = 'Enter "c" to create a blog, "l" to list them, "r" to read one, "p" to write a post, or "q" to quit: '
+POST_TEMPLATE = '''
+--- {} ---
+
+{}
+
+'''
+
 blogs = dict() # blog_name: Blog obj
 
 def menu():
     # Show the user the available blogs
     # Let the user make a choice
     print_blogs()
+    selection = input(MENU_PROMPT)
+    while (selection != 'q'):
+        if selection == 'c':
+            ask_create_blog()
+        elif selection == 'l':
+            print_blogs()
+        elif selection == 'r':
+            ask_read_blog()
+        elif selection == 'p':
+            ask_create_post()
+        selection = input(MENU_PROMPT)
 
 def print_blogs():
     # Print the available blogs
-    print("Blogs!")
     for key, blog in blogs.items(): # [(blog_name, Blog), ... (blog_name_n, Blog_n)]
         print('- {}'.format(blog))
+
+def ask_create_blog():
+    title = input('Enter your blog title: ')
+    author = input('Enter your name: ')
+    blogs[title] = Blog(title, author)
+
+
+def ask_read_blog():
+    title = input('Enter the blog title you want to read: ')
+    print_posts(blogs[title])
+
+def print_posts(blog):
+    for post in blog.posts:
+        print_post(post)
+
+def print_post(post):
+    print(POST_TEMPLATE.format(post.title, post.content))
+
+def ask_create_post():
+    pass
